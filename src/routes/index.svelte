5c0510927c8464175e611ee94ab26e2758ee7383
ReeyaB123 <reeya4151@gmail.com>
2022-01-16 02:35:19 +0000

    commit4

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script>
     import supabase from '$lib/db';
-    import Signup from '$lib/signup.svelte';
+    
 
     async function logout() {
    	 const { error } = await supabase.auth.signOut();
@@ -10,13 +10,8 @@
 
 </script>
 
-<style>
-    @import
-   url("https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css");
-   </style>
    
 <!-- Sign Out -->
 <section class="container px-4 py-3 text-center">
     <button class="btn btn-secondary" on:click={logout}>Logout</button>
 </section>
-<Signup/>
